4d11c6dac9917de256455ee0eded33ab198ed014
abdullahakyuz <131802809+abdullahakyuz@users.noreply.github.com>
2024-04-18 04:35:12 +0300

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-ai-backend" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "abdu-key"
     region = "us-east-1"
   }
 }
