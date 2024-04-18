e72cd98c76890755020416542423bb505922aab4
abdullahakyuz <131802809+abdullahakyuz@users.noreply.github.com>
2024-04-18 03:57:49 +0300

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "abdu-backend" # Replace with your actual S3 bucket name
+    bucket = "gpt-ai-backend" # Replace with your actual S3 bucket name
     key    = "abdu-key"
     region = "ap-south-1"
   }
